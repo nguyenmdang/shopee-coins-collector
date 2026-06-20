8aa75979cca0bf27c91a86e4be25f30ff1b69296
Nguyen Minh Dangggg <109588838+nguyenmdang@users.noreply.github.com>
2026-06-20 19:24:36 +0700

    Update collect.ts

--- collect.ts
+++ collect.ts
@@ -1,29 +1,37 @@
-import { PuppeteerLaunchOptions } from 'puppeteer-core';
-import { getCookies } from './lib/config';
-import { BaseCrawler } from './lib/crawlers/BaseCrawler';
-import { ShopeeCrawler } from './lib/crawlers/ShopeeCrawler';
-
-async function main () {
-  const opt: PuppeteerLaunchOptions = {
-    headless: true,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-gpu',
-      '--disable-accelerated-2d-canvas',
-      '--disable-pdf-viewer',
-      '--disable-features=IsolateOrigins,site-per-process'
-    ],
-    // Chỉ định executable path cho Chromium cài qua apt
-    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/chromium-browser'
-  };
-
-  const crawlers: BaseCrawler[] = [
-    new ShopeeCrawler(opt, { }, getCookies())
-  ];
-
-  await Promise.all(crawlers.map(cr => cr.run()));
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const config_1 = require("./lib/config");
+const ShopeeCrawler_1 = require("./lib/crawlers/ShopeeCrawler");
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        // Cấu hình với đầy đủ flags
+        const opt = {
+            headless: true,
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-gpu',
+                '--disable-accelerated-2d-canvas',
+                '--disable-pdf-viewer',
+                '--disable-features=IsolateOrigins,site-per-process'
+            ],
+            // Sử dụng Chromium từ hệ thống nếu có
+            executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined
+        };
+        const crawlers = [
+            new ShopeeCrawler_1.ShopeeCrawler(opt, {}, (0, config_1.getCookies)())
+        ];
+        yield Promise.all(crawlers.map(cr => cr.run()));
+    });
 }
-
-(async () => main())();
+(() => main())();
